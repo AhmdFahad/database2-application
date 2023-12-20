cb2bbc5430cad1ae7c87670bd60d8fa464087788
AhmdFahad <ahmd66699@gmail.com>
2023-12-20 14:46:35 +0300

    create staff

---
+++ src/main/java/org/ahamdah/database2application/model/Staff.java
@@ -0,0 +1,31 @@
+package org.ahamdah.database2application.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "staff")
+public class Staff {
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "position")
+    private String position;
+
+    @Column(name = "email")
+    private String email;
+
+}
